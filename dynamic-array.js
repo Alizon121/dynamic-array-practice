334c7b3495809952551351a6ee3bb45ea60e13f4
Andrew <andrew.lizon12@gmail.com>
2024-05-08 16:00:39 -0700

    Complete

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,19 +1,29 @@
 class DynamicArray {
-
-  constructor(defaultSize=4) {
-
-    // Your code here 
-
+  constructor(capacity = 4) {
+    this.length = 0
+    this.capacity = capacity
+    this.data = new Array(this.capacity)
   }
 
   read(index) {
+// We need to iterate through the array and return i
+for (let i = 0; i <= this.data.length; i++) {
+  return this.data[index]
+}
 
-    // Your code here 
   }
 
   unshift(val) {
+// We need to add values to the front using unshift method
+// We need a way to add val to the front of array
+// The array we have is this.data, which also has a capacity initialized
 
-    // Your code here 
+for (let i = this.length; i > 0; i--) {
+this.data[i] = this.data[i - 1]
+}
+this.data[0] = val
+  this.length++;
+
   }
 
 }
